82155e3753781e22e6fc8afa62d843aa4a81e081
saigowthamr <saigowthamr@gmail.com>
2018-09-11 11:25:33 +0530

    qa changed

--- src/qa.js
+++ src/qa.js
@@ -2,7 +2,7 @@ const all = [
     {
         question: "What is react?",
         answer:
-            "React is a JavaScript library used to build views which is only concentrated on the view part in the MVC model."
+            "React is a JavaScript library used to build views which is mainly concentrated on the view part in the MVC model."
     },
     {
         question: "What is JSX ?",
@@ -12,16 +12,12 @@ const all = [
     {
         question: "What is Nodejs  ?",
         answer:
-            "Nodejs is a backend JavaScript framework built on top of v8 JavaScript engine. By using node js you can built any kind of backend stuff."
+            "Nodejs is a backend JavaScript framework built on top of v8 JavaScript engine."
     },
     {
         question: "What is npm ?",
         answer:
             "npm is a node package manager which is used to install the libraries created by the other people.By using npm you can install or uninstall packages at any point of time."
-    },
-    {
-        question: "What is react ?",
-        answer: "A JavaScript library for building user interfaces"
     }
 ];
 
